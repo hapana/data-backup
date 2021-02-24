d0fb3673f9ffb5d7cd7cc250ab8f857bff851163
Harry P <hpanay@gmail.com>
2021-02-24 12:41:25 +0000

    Sort out directories and orchestrate backups appropriately

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module data-backup
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.3.0
+require (
+	github.com/otiai10/copy v1.5.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -11,13 +11,19 @@ import (
 
 	"os/exec"
 
+	"github.com/otiai10/copy"
 	"github.com/urfave/cli/v2"
 )
 
 type backupConfig struct {
-	Directory     string
+	Directories   directory
 	DockerCompose bool
-	DataDirectory string
+}
+
+type directory struct {
+	Root   string
+	Data   string
+	Backup string
 }
 
 var Platforms = map[string]string{
@@ -30,7 +36,9 @@ func main() {
 		Usage: "backup data with complex process",
 		Action: func(c *cli.Context) error {
 			config := backupConfig{
-				Directory: c.String("path"),
+				Directories: directory{
+					Root: c.String("path"),
+				},
 			}
 			err := config.backup()
 			if err != nil {
@@ -40,10 +48,11 @@ func main() {
 		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:    "path",
-				Value:   "path",
-				Usage:   "Path of the config file",
-				Aliases: []string{"p"},
+				Name:        "path",
+				Value:       "path",
+				Usage:       "Path of the config file",
+				DefaultText: ".",
+				Aliases:     []string{"p"},
 			},
 		},
 	}
@@ -57,7 +66,7 @@ func main() {
 func (c *backupConfig) backup() error {
 	var platformImplemented string
 
-	log.Printf("Read config file: %v", c.Directory)
+	log.Printf("Read config file: %v", c.Directories.Data)
 	err := c.readConfig()
 	if err != nil {
 		return err
@@ -74,7 +83,7 @@ func (c *backupConfig) backup() error {
 		}
 	}
 
-	err = c.processFile(filepath.Dir(c.Directory), platformImplemented)
+	err = c.processFile(filepath.Dir(c.Directories.Data), platformImplemented)
 	if err != nil {
 		return err
 	}
@@ -84,27 +93,24 @@ func (c *backupConfig) backup() error {
 
 func (c *backupConfig) readConfig() error {
 	const filename = ".backup.json"
-	var filepath = filepath.Join(c.Directory, filename)
-	var config = backupConfig{}
+	var filepath = filepath.Join(c.Directories.Root, filename)
 
 	bytes, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return err
 	}
 
-	err = json.Unmarshal(bytes, &config)
+	err = json.Unmarshal(bytes, &c)
 	if err != nil {
 		return err
 	}
 
-	c.DataDirectory = config.Directory
-
-	log.Printf("Config is: %+v", config)
+	log.Printf("Config is: %+v", c)
 	return nil
 }
 
 func (c *backupConfig) findFile(filePattern string) (bool, error) {
-	dir := filepath.Dir(c.Directory)
+	dir := filepath.Dir(c.Directories.Root)
 	var found bool
 
 	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
@@ -135,16 +141,33 @@ func (c *backupConfig) findFile(filePattern string) (bool, error) {
 func (c *backupConfig) processFile(dataDir, platform string) error {
 	switch platform {
 	case "docker":
+		log.Println("Pulling down docker-compose")
 		cmd := exec.Command(
 			"docker-compose",
 			"down",
 		)
-		cmd.Dir = c.Directory
-		stdoutStderr, err := cmd.CombinedOutput()
+		cmd.Dir = c.Directories.Root
+		_, err := cmd.CombinedOutput()
 		if err != nil {
 			return err
 		}
-		log.Printf("Output is: %s\n", stdoutStderr)
+		log.Printf("Copying data from %s to %s", c.Directories.Data, c.Directories.Backup)
+		err = copy.Copy(filepath.Join(c.Directories.Root, c.Directories.Data), filepath.Join(c.Directories.Root, c.Directories.Backup))
+		if err != nil {
+			return err
+		}
+
+		log.Println("Starting docker-compose again")
+		cmd = exec.Command(
+			"docker-compose",
+			"up",
+			"-d",
+		)
+		cmd.Dir = c.Directories.Root
+		_, err = cmd.CombinedOutput()
+		if err != nil {
+			return err
+		}
 	default:
 		return fmt.Errorf("No platform specified\n")
 	}
